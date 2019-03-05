56a193b4b8faeacaca846281d1855d8fc7b7025f
sdymj84 <sdymj84@gmail.com>
2019-03-04 22:56:32 -0600

    Update functions

--- get.js
+++ get.js
@@ -1,15 +1,14 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context) {
+export async function resident(event, context) {
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.residentsTable,
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
+      residentId: event.requestContext.identity.cognitoIdentityId,
     }
   };
 
--- list.js
+++ list.js
@@ -1,23 +1,24 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context) {
+export async function resident(event, context) {
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.residentsTable,
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
-    KeyConditionExpression: "userId = :userId",
-    ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId
-    }
+
+    // KeyConditionExpression: "residentId = :residentId",
+    // ExpressionAttributeValues: {
+    //   ":residentId": event.requestContext.identity.cognitoIdentityId
+    // }
   };
 
   try {
-    const result = await dynamoDbLib.call("query", params);
+    const result = await dynamoDbLib.call("scan", params);
     // Return the matching list of items in response body
     return success(result.Items);
   } catch (e) {
