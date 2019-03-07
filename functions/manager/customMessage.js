9654be5553e67241975e43f8d7c02f8a0b803201
sdymj84 <sdymj84@gmail.com>
2019-03-06 20:22:24 -0600

    Update customMessage to work

--- functions/manager/customMessage.js
+++ functions/manager/customMessage.js
@@ -1,8 +1,6 @@
-import { success, failure } from "../../libs/response-lib";
-
 export function main(event, context, callback) {
   console.log("custom message handler triggered", event)
-  /* if (event.triggerSource === "CustomMessage_SignUp") {
+  if (event.triggerSource === "CustomMessage_SignUp") {
     // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
     const { codeParameter } = event.request
     const { userName, region } = event
@@ -12,7 +10,7 @@ export function main(event, context, callback) {
     const link = `<a href="${url}?code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}" target="_blank">here</a>`
     event.response.emailSubject = "Your verification link"; // event.request.codeParameter
     event.response.emailMessage = `Thank you for signing up. Click ${link} to verify your email.`;
-  } */
-
+  }
   callback(null, event)
+
 }
