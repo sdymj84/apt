e0821d99a60e8682c43ea1b78aa0a6352e41dd0d
sdymj84 <sdymj84@gmail.com>
2019-04-07 21:10:47 -0500

    Add listRequests, updateNote in request

---
+++ functions/maintanances/list.js
@@ -0,0 +1,23 @@
+/* Use Case  
+1. /requests/list
+  -> Manager see list of requests
+  -> In client, data will be filtered/sorted by priority/apart/data..
+
+*/
+
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function request(event, context) {
+  const params = {
+    TableName: process.env.maintanancesTable,
+  }
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    return success(result);
+  } catch (e) {
+    console.log(e)
+    return failure({ status: false, error: e });
+  }
+}
---
+++ functions/maintanances/updateNote.js
@@ -0,0 +1,34 @@
+/* Use Case  
+1. Manager update note with requestId
+
+*/
+
+
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function request(event, context) {
+  const data = JSON.parse(event.body)
+  const params = {
+    TableName: process.env.maintanancesTable,
+    Key: {
+      requestId: event.pathParameters.requestId,
+      apartId: data.apartId
+    },
+    UpdateExpression:
+      "SET maintananceNote = :maintananceNote",
+    ExpressionAttributeValues: {
+      ":maintananceNote": data.maintananceNote
+    },
+    ReturnValues: "ALL_NEW",
+  }
+
+  try {
+    const result = await dynamoDbLib.call("update", params);
+    return success(result);
+
+  } catch (e) {
+    console.log(e)
+    return failure({ status: false, error: e });
+  }
+}
